db6ececc7213e715d65c0dad55baeff0927a76db
burpmonster <lucas.c.haskell@gmail.com>
2023-06-27 15:59:10 -0400

    push for test

--- api/main.py
+++ api/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import os
+from authenticator import authenticator
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers import auth
+from routers import accounts
 
 app = FastAPI()
-
+app.include_router(authenticator.router)
 
 app.add_middleware(
     CORSMiddleware,
