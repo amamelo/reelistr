1c11c02211b5ac791c1fbec1d0641b10431cc16a
Lucas Haskell <lucas.c.haskell@gmail.com>
2023-06-29 21:44:05 +0000

    Luke

--- api/main.py
+++ api/main.py
@@ -1,9 +1,12 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import os
+from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
+from routers.authenticator import authenticator
+from routers import accounts
+
 
 app = FastAPI()
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -27,3 +30,6 @@ def launch_details():
             "min": "00"
         }
     }
+
+
+app.include_router(authenticator.router)
---
+++ api/routers/accounts.py
@@ -0,0 +1,51 @@
+from fastapi import (
+    Depends,
+    HTTPException,
+    status,
+    Response,
+    APIRouter,
+    Request,
+)
+from jwtdown_fastapi.authentication import Token
+from routers import authenticator
+
+from pydantic import BaseModel
+
+from queries.accounts import (
+    UserIn,
+    UserOut,
+    UserRepo,
+    DuplicateAccountError,
+)
+
+class AccountForm(BaseModel):
+    username: str
+    password: str
+
+class AccountToken(Token):
+    account: UserOut
+
+class HttpError(BaseModel):
+    detail: str
+
+router = APIRouter()
+
+
+@router.post("/api/accounts", response_model=AccountToken | HttpError)
+async def create_account(
+    info: UserIn,
+    request: Request,
+    response: Response,
+    repo: UserRepo = Depends(),
+):
+    hashed_password = authenticator.hash_password(info.password)
+    try:
+        account = repo.create(info, hashed_password)
+    except DuplicateAccountError:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cannot create an account with those credentials",
+        )
+    form = AccountForm(username=info.email, password=info.password)
+    token = await authenticator.login(response, request, form, repo)
+    return AccountToken(account=account, **token.dict())
