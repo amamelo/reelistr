4c225f25bc1e624dab28b293bc37889cffdf155c
burpmonster <lucas.c.haskell@gmail.com>
2023-07-12 17:22:13 -0400

    add trending, upcoming routes

--- ghi/src/components/MovieSearch.js
+++ ghi/src/components/MovieSearch.js
@@ -62,7 +62,7 @@ const Movie = () => {
                     <Card.Img variant='top' src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" + movie.poster_path} alt="Movie Poster" style={{ height: '300px' }} />
                     <Card.Body>
                       <Card.Text style={{ fontSize: '16px'}}>{movie.overview}</Card.Text>
-                      <Button variant='primary' href='#'>Go To Movie</Button>
+                      <Button variant='primary' href={'http://localhost:8000/movies/details/' + movie.id}>Go To Movie</Button>
                     </Card.Body>
                   </Card>
                 </Col>
