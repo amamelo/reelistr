ccfa650c0bcfcb408784e488181d1669d5e066d8
dan barker <dbarker16@gmail.com>
2023-07-14 17:54:48 -0400

    excalidraw done

--- api/migrations/04_watchlist_table.py
+++ api/migrations/04_watchlist_table.py
@@ -4,7 +4,7 @@ steps = [
     CREATE TABLE watchlist (
         id SERIAL PRIMARY KEY NOT NULL,
         username VARCHAR(50) UNIQUE NOT NULL REFERENCES accounts(username),
-        movie_id INTEGER NOT NULL
+        movie_id INTEGER NOT NULL REFERENCES movies(tmdb_movie_id)
     );
     """,
     """
