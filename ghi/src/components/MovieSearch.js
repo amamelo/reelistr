694dea57536925d4b4d74b874b7e8917c02fc28e
Angelica Melo <angelicamm10@gmail.com>
2023-07-25 11:57:55 -0400

    merged main into this one

--- ghi/src/components/MovieSearch.js
+++ ghi/src/components/MovieSearch.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
+import { Link } from "react-router-dom"
 
 const Movie = () => {
   const [title, setTitle] = useState("");
@@ -57,7 +58,9 @@ const Movie = () => {
                 <Col xs='4'>
                   <Card key={movie.poster_path} style={{ width: '18rem' }} className="flex-fill">
                     <Card.Header style={{ fontSize: '26px' }}>{movie.title}</Card.Header>
-                    <Card.Img variant='top' src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" + movie.poster_path} alt="Movie Poster" style={{ height: '300px' }} />
+                    <Link to="/movies/">
+                      <Card.Img variant='top' src={"https://www.themoviedb.org/t/p/w600_and_h900_bestv2/" + movie.poster_path} alt="Movie Poster" style={{ height: '300px' }} />
+                    </Link>
                     <Card.Body>
                       <Card.Text style={{ fontSize: '16px' }}>{movie.overview}</Card.Text>
                       <Button variant='primary' href={'http://localhost:3000/movies/' + movie.id}>Go To Movie</Button>
@@ -70,7 +73,7 @@ const Movie = () => {
         </Container>
       </div>
 
-    </div>
+    </div >
   );
 };
 
