a9f4cf4a68181cb1169d89009f6834b810b6f6e2
Rey <reyxeka@gmail.com>
2023-07-26 19:59:20 -0400

    Navbar fully complete

--- ghi/src/navigation/Nav.js
+++ ghi/src/navigation/Nav.js
@@ -1,8 +1,8 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../css/styles.css";
 import { NavLink } from 'react-router-dom';
 import { useState, useEffect } from "react";
-import Movie from "../components/MovieSearch";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 
 
@@ -16,89 +16,69 @@ function Nav() {
     }
   }, [token]);
 
+
   function logout() {
-    localStorage.removeItem("token");
+    localStorage.setItem("token", null);
     setIsLoggedIn(false);
   }
+
+
   return (
-    <div className="navbar">
-      <div className="navbar-container">
-        <NavLink className="logo-container" to="/"><h1 className="logo">reelistr</h1></NavLink>
-        <div className="menu-container">
+    <nav className="navbar custom-navbar navbar-expand-lg navbar-dark">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand logo" to="/"><div className="logo">Reelister</div></NavLink>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item dropdown">
+              <NavLink className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Movies
+              </NavLink>
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><NavLink className="dropdown-item" to="/trending">Trending</NavLink></li>
+                <li><NavLink className="dropdown-item" to="/upcoming">Coming Soon</NavLink></li>
+              </ul>
+            </li>
+            {isLoggedIn ? (
+              <li className="nav-item dropdown">
+                <NavLink className="nav-link dropdown-toggle" to="/user/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Profile
+                </NavLink>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><NavLink className="dropdown-item" to="/user/profile">My Profile</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="/user/collections">Collections</NavLink></li>
+                  <li><NavLink className="dropdown-item" to="/user/library">Library</NavLink></li>
+                </ul>
+              </li>
+            ) : null}
+            {isLoggedIn ? (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/watchlist">Watchlist</NavLink>
+              </li>
+            ) : null}
+          </ul>
           {isLoggedIn ? (
-            <ul className="menu-list">
-              <NavLink className="nav-link" to="/"><li className="menu-list-item">Home</li></NavLink>
-              <NavLink className="nav-link"><li className="menu-list-item">Movies</li></NavLink>
-              <NavLink className="nav-link" to="/watchlist"><li className="menu-list-item">Watchlist</li></NavLink>
-              <NavLink className="nav-link" to="/profile"><li className="menu-list-item">Profile</li></NavLink>
-              <NavLink className="nav-link" onClick={logout}><li className="menu-list-item">Logout</li></NavLink>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" onClick={logout} to="/">Logout</NavLink>
+              </li>
             </ul>
           ) : (
-            // Links to display when the user is not signed in
-            <div className="menu-container">
-              <ul className="menu-list">
-                <NavLink className="nav-link" to="/"><li className="menu-list-item">Home</li></NavLink>
-                <NavLink classname="nav-link" to="/comingsoon"><li className="menu-list-item">Movies</li></NavLink>
-                <NavLink classname="nav-link" to="/login"><li className="menu-list-item">Login</li></NavLink>
-                <NavLink className="nav-link" to="/signup"><li className="menu-list-item">Sign Up</li></NavLink>
-              </ul>
-            </div>
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">Register</NavLink>
+              </li>
+            </ul>
           )}
         </div>
-        <div className="search-container"><li className="search-bar"><Movie /></li></div>
       </div>
-    </div>
-  )
+    </nav>
+  );
 }
 
 export default Nav;
-
-
-// return (
-//   <div className="navbar">
-//     <div className="navbar-container">
-//       <NavLink className="navbar-brand" to="/">Reelistr</NavLink>
-//       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-//         <span className="navbar-toggler-icon"></span>
-//       </button>
-//       <div className="collapse navbar-collapse" id="navbarNavDropdown">
-//         <ul className="navbar-nav">
-//           <li className="nav-item">
-//             <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
-//           </li>
-//           <li className="nav-item">
-//             <li><NavLink className="nav-link" aria-current="page" to="/signup">Sign Up</NavLink></li>
-//           </li>
-//           <li className="nav-item">
-//             <NavLink className="nav-link" aria-current="page" to="/login">Log In</NavLink>
-//           </li>
-//           <li className="nav-item">
-//             <li><NavLink className="nav-link" aria-current="page" to="/watchlist">Watchlist</NavLink></li>
-//           </li>
-//           <ul className="navbar-nav">
-//             <li className="nav-item dropdown">
-//               <NavLink className="nav-link dropdown-toggle" aria-current="page" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="/movies/">Movies</NavLink>
-//               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <li><NavLink className="dropdown-item" aria-current="page" to="/movies/comingsoon">Coming Soon</NavLink></li>
-//                 <li><NavLink className="dropdown-item" aria-current="page" to="/movies/trending">Trending</NavLink></li>
-//               </ul>
-//             </li>
-//           </ul>
-//           <ul className="navbar-nav">
-//             <li className="nav-item dropdown">
-//               <NavLink className="nav-link dropdown-toggle" aria-current="page" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" to="/user/">Profile</NavLink>
-//               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                 <NavLink className="dropdown-item" aria-current="page" to="/user/profile">My Profile</NavLink>
-//                 <li><NavLink className="dropdown-item" aria-current="page" to="/user/collections">Collections</NavLink></li>
-//                 <li><NavLink className="dropdown-item" aria-current="page" to="/user/library">Library</NavLink></li>
-
-//               </ul>
-//             </li>
-//           </ul>
-//         </ul>
-//         <li className="nav-link">
-//           <Movie />
-//         </li>
-//       </div>
-//     </div>
-//   </div>
