0a126722b4db14513c4e269b8086ee14c02728e5
burpmonster <lucas.c.haskell@gmail.com>
2023-07-26 18:16:17 -0400

    comment out unused function for linter testing

--- ghi/src/pages/Watchlist.js
+++ ghi/src/pages/Watchlist.js
@@ -24,32 +24,33 @@ export default function Watchlist() {
         }
     }
 
-    const fetchMovies = async () => {
-        const watchlistUrl = `http://localhost:8000/users/${username}/watchlist/${watchlist_id}/`;
-        const response = await fetch(watchlistUrl, { headers: { Authorization: `Bearer ${token}` }, })
-        if (response.ok) {
-            const data = await response.json();
-            setMovies(data)
+    // remove before project submission
+    // const fetchMovies = async () => {
+    //     const watchlistUrl = `http://localhost:8000/users/${username}/watchlist/${watchlist_id}/`;
+    //     const response = await fetch(watchlistUrl, { headers: { Authorization: `Bearer ${token}` }, })
+    //     if (response.ok) {
+    //         const data = await response.json();
+    //         setMovies(data)
 
 
-            // extract movie IDs
-            const movieIds = data.map(movie => movie.movie_id);
+    //         // extract movie IDs
+    //         const movieIds = data.map(movie => movie.movie_id);
 
-            // fetch movie details for each movie
-            const posterPaths = []
-            for (const movieId of movieIds) {
-                const movieUrl = `http://localhost:8000/movies/details/${movieId}`;
-                const movieResponse = await fetch(movieUrl);
-                if (movieResponse.ok) {
-                    const data = await movieResponse.json();
-                    posterPaths.push(data.poster_path)
-                }
-            }
-            setPosterPaths(posterPaths)
-        } else {
-            throw new Error("Failed to retrieve movies watched")
-        }
-    };
+    //         // fetch movie details for each movie
+    //         const posterPaths = []
+    //         for (const movieId of movieIds) {
+    //             const movieUrl = `http://localhost:8000/movies/details/${movieId}`;
+    //             const movieResponse = await fetch(movieUrl);
+    //             if (movieResponse.ok) {
+    //                 const data = await movieResponse.json();
+    //                 posterPaths.push(data.poster_path)
+    //             }
+    //         }
+    //         setPosterPaths(posterPaths)
+    //     } else {
+    //         throw new Error("Failed to retrieve movies watched")
+    //     }
+    // };
 
     useEffect(() => {
         if (username !== '') {
