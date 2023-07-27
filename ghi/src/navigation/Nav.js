e1c8155b611635290a9138a7104433d116b77bda
Lucas Haskell <lucas.c.haskell@gmail.com>
2023-07-27 18:39:33 +0000

    Cicd edits

--- ghi/src/navigation/Nav.js
+++ ghi/src/navigation/Nav.js
@@ -71,7 +71,7 @@ function Nav() {
                 <NavLink className="nav-link" to="/login">Login</NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to="/register">Register</NavLink>
+                <NavLink className="nav-link" to="/signup">Register</NavLink>
               </li>
             </ul>
           )}
