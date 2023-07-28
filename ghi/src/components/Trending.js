9b4dc202999f4c7710505ab2a9431659b6a17f66
burpmonster <lucas.c.haskell@gmail.com>
2023-07-28 12:17:43 -0400

    moved fetch functions inside useEffect + dependency

--- ghi/src/components/Trending.js
+++ ghi/src/components/Trending.js
@@ -10,19 +10,20 @@ function TrendingApi() {
   const [movies, setMovies] = useState([]);
   const baseUrl = process.env.REACT_APP_API_HOST
 
-  const fetchTrending = async () => {
-    const response = await fetch(`${baseUrl}/trending/`);
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data)
-      return setMovies(data);
-    }
-    throw new Error("Failed to fetch movie details");
-  };
+
 
   useEffect(() => {
+    const fetchTrending = async () => {
+      const response = await fetch(`${baseUrl}/trending/`);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        return setMovies(data);
+      }
+      throw new Error("Failed to fetch movie details");
+    };
     fetchTrending();
-  }, []);
+  }, [baseUrl]);
 
   return (
     <div>
