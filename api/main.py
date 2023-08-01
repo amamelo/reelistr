d3a1d309adcd91acab2a51befa2733c607377695
burpmonster <lucas.c.haskell@gmail.com>
2023-08-01 17:54:53 -0400

    removed site.webmanifest from index.html

--- api/main.py
+++ api/main.py
@@ -14,7 +14,7 @@ from routers import (
 
 origins = [
     os.environ.get("CORS_HOST"),
-    "https://localhost:3000",
+    "http://localhost:3000",
     'https://barkmulcher.gitlab.io/module3-project-gamma',
     'https://mar-2-et-reelistr.mod3projects.com'
 ]
